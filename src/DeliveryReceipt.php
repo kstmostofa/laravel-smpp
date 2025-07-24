f0a9d798f32a0429524b64d1c3c72bb93942093a
kstmostofa <mostofa122@gmail.com>
2025-07-24 17:09:28 +0600

    Version: 1.0.3

--- src/DeliveryReceipt.php
+++ src/DeliveryReceipt.php
@@ -3,12 +3,21 @@
 
 namespace Kstmostofa\LaravelSmpp;
 
+use DateTime;
+
 /**
  * An extension of a SMS, with data embedded into the message part of the SMS.
  * @author mostofa122@gmail.com
  */
 class DeliveryReceipt extends Sms
 {
+    // Parsed DLR data
+    public $messageId;
+    public $status;
+    public $finalDate;
+    public $errorCode;
+
+    // Raw DLR data
     public $id;
     public $sub;
     public $dlvrd;
@@ -26,16 +35,45 @@ class DeliveryReceipt extends Sms
      */
     public function parseDeliveryReceipt()
     {
-        $numMatches = preg_match('/^id:([^ ]+) sub:(\d{1,3}) dlvrd:(\d{3}) submit date:(\d{10,12}) done date:(\d{10,12}) stat:([A-Z ]{7}) err:(\d{2,3}) text:(.*)$/si', $this->message, $matches);
+        $pattern = '/^id:(?P<id>[^ ]+) sub:(?P<sub>\d{1,3}) dlvrd:(?P<dlvrd>\d{3}) submit date:(?P<submitDate>\d{10,12}) done date:(?P<doneDate>\d{10,12}) stat:(?P<stat>[A-Z ]{7}) err:(?P<err>\d{2,3}) text:(?P<text>.*)$/si';
+        $numMatches = preg_match($pattern, $this->message, $matches);
+
         if ($numMatches == 0) {
-            throw new \InvalidArgumentException('Could not parse delivery receipt: '.$this->message."\n".bin2hex($this->body));
+            // Try a more flexible regex for cases where 'text' might be missing or format varies slightly
+            $pattern = '/id:(?P<id>\S+)\s+sub:(?P<sub>\d+)\s+dlvrd:(?P<dlvrd>\d+)\s+submit date:(?P<submitDate>\d+)\s+done date:(?P<doneDate>\d+)\s+stat:(?P<stat>\w+)\s+err:(?P<err>\d+)/i';
+            $numMatches = preg_match($pattern, $this->message, $matches);
+            if ($numMatches == 0) {
+                throw new \InvalidArgumentException('Could not parse delivery receipt: ' . $this->message . "\n" . bin2hex($this->body));
+            }
         }
-        list($matched, $this->id, $this->sub, $this->dlvrd, $this->submitDate, $this->doneDate, $this->stat, $this->err, $this->text) = $matches;
+
+        $this->id = $matches['id'];
+        $this->sub = $matches['sub'];
+        $this->dlvrd = $matches['dlvrd'];
+        $this->submitDate = $matches['submitDate'];
+        $this->doneDate = $matches['doneDate'];
+        $this->stat = $matches['stat'];
+        $this->err = $matches['err'];
+        $this->text = $matches['text'] ?? ''; // Handle missing text part
+
+        // Map to user-friendly properties
+        $this->messageId = $this->id;
+        $this->status = $this->stat;
+        $this->errorCode = $this->err;
 
         // Convert dates
-        $dp = str_split($this->submitDate,2);
-        $this->submitDate = gmmktime($dp[3],$dp[4],isset($dp[5]) ? $dp[5] : 0,$dp[1],$dp[2],$dp[0]);
-        $dp = str_split($this->doneDate,2);
-        $this->doneDate = gmmktime($dp[3],$dp[4],isset($dp[5]) ? $dp[5] : 0,$dp[1],$dp[2],$dp[0]);
+        if ($this->doneDate) {
+            $dp = str_split($this->doneDate, 2);
+            // YYMMDDHHII[SS]
+            $year = $dp[0];
+            $month = $dp[1];
+            $day = $dp[2];
+            $hour = $dp[3];
+            $minute = $dp[4];
+            $second = $dp[5] ?? '00';
+            $this->finalDate = DateTime::createFromFormat('y-m-d H:i:s', "$year-$month-$day $hour:$minute:$second");
+        } else {
+            $this->finalDate = null;
+        }
     }
 }
